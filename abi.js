1e7fc5dbc5aabb4fd49d4fd4102bcc62b2886187
jin1963 <kaojino89@gmail.com>
2025-06-15 15:10:05 +0700

    Delete abi.js

--- abi.js
+++
@@ -1,5 +0,0 @@
-const stakingABI = [ ... (ใช้ชุด ABI Ultra เดิมล่าสุด) ... ];
-const erc20ABI = [
-  {"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_value","type":"uint256"}],"name":"approve","outputs":[{"name":"","type":"bool"}],"type":"function"},
-  {"constant":true,"inputs":[{"name":"owner","type":"address"},{"name":"spender","type":"address"}],"name":"allowance","outputs":[{"name":"","type":"uint256"}],"type":"function"}
-];
